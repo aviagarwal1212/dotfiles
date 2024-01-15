121b60c38135b3fe7f9079a6bfdff84530716bfe
Avi Agarwal <aviagarwal1212@gmail.com>
2024-01-15 16:47:23 +0000

    update

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,78 +1,79 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
-    config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = {
-          "lua_ls",        --lua
-          "pyright",       --python
-          "ruff_lsp",      --python
-          "bashls",        --bash
-          "dockerls",      --docker
-          "gopls",         --go
-          "html",          --html
-          "htmx",          --htmx
-          "jsonls",        --json
-          "rust_analyzer", --rust
-          "sqlls",         --sql
-          "svelte",        --svelte
-          "taplo",         --toml
-          "tailwindcss",   --tailwind
-          "terraformls",   --terraform
-          "tsserver",      --typescript
-          "zls",           --zig
-        },
-        automatic_installation = true,
-      }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			require("mason-lspconfig").setup {
+				ensure_installed = {
+					"lua_ls", --lua
+					"pyright", --python
+					"ruff_lsp", --python
+					"bashls", --bash
+					"dockerls", --docker
+					"gopls", --go
+					"html", --html
+					"htmx", --htmx
+					"jsonls", --json
+					"rust_analyzer", --rust
+					"sqlls", --sql
+					"svelte", --svelte
+					"taplo", --toml
+					"tailwindcss", --tailwind
+					"terraformls", --terraform
+					"tsserver", --typescript
+					"zls", --zig
+				},
+				automatic_installation = true,
+			}
 
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      require("mason-lspconfig").setup_handlers {
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities
-          })
-        end,
-        function()
-          require("rust_analyzer").setup({
-            capabilities = capabilities,
-            settings = {
-              checkOnSave = {
-                allFeatures = true,
-                command = "clippy",
-                extraArgs = { "--no-deps" },
-              }
-            }
-          })
-        end
-      }
-    end
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim"
-    },
-    config = function()
-      require("null-ls").setup({
-        sources = {
-          require("null-ls").builtins.diagnostics.mypy,
-          require("null-ls").builtins.formatting.prettier
-        }
-      })
-      require("mason-null-ls").setup({
-        ensure_installed = nil,
-        automatic_installation = true,
-      })
-    end
-  }
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("mason-lspconfig").setup_handlers {
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities
+					})
+				end,
+				function()
+					require("rust_analyzer").setup({
+						capabilities = capabilities,
+						settings = {
+							checkOnSave = {
+								allFeatures = true,
+								command = "clippy",
+								extraArgs = { "--no-deps" },
+							}
+						}
+					})
+				end
+			}
+		end
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim"
+		},
+		config = function()
+			require("null-ls").setup({
+				sources = {
+					require("null-ls").builtins.diagnostics.mypy,
+					require("null-ls").builtins.formatting.prettier,
+					require("null-ls").builtins.diagnostics.golangci_lint,
+				}
+			})
+			require("mason-null-ls").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+		end
+	}
 }
